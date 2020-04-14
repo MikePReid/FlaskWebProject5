317e245e5f9c205960295201c16a0a01bbab8a9b
Mike Reid <mikerinar@gmal.com>
2020-04-13 21:40:04 -0400

    Blockchain endpoint built

--- FlaskWebProject5/HelloFlask/views.py
+++ FlaskWebProject5/HelloFlask/views.py
@@ -2,10 +2,14 @@ from datetime import datetime
 from flask import render_template, jsonify
 from HelloFlask import app
 import json
+from uuid import uuid4
 from HelloFlask.models import BlockChain
 
 # Instantiate the Blockchain
 blockchain = BlockChain()
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
 
 @app.route('/')
 @app.route('/home')
@@ -25,37 +29,72 @@ def get_data():
 
 @app.route('/mine', methods=['GET'])
 def mine():
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+        )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+        }
     return render_template(
         "BlockChain.html",
         title = "Block Chain - Mine",
-        content = "We'll mine a new Block")
+        content = response)
 
 
-@app.route('/transactions/new', methods=['GET'])  #<-- Change to POST
+@app.route('/transactions/new', methods=['POST'])  #<-- Change to POST
 def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+         return 'Missing values', 400
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction will be added to Block {index}'}
+
     return render_template(
         "BlockChain.html",
         title = "Block Chain - New Transaction",
-        content = "We'll add a new transaction")
+        content = reposnse,
+#        content = jsonify(reposnse),
+        )
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
-    print(jsonify({
+    print({
             'chain': blockchain.chain,
             'length': len(blockchain.chain)
-            })
+            }
           )
-#    response = {
-#        'chain': blockchain.chain,
-#        'length': len(blockchain.chain),
-#        }
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+        }
     return render_template(
         "BlockChain.html",
         title = "Block Chain - The Chain",
-        content = jsonify({
-            'chain': blockchain.chain,
-            'length': len(blockchain.chain),
-        }))
+        content = response,
+#        content = jsonify(response)
+        )
 
 @app.route('/about')
 def about():
